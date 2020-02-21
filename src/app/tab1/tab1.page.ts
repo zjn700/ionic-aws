7c07e1645906f86e3b53b9223adfe092b8503838
Ubuntu <ubuntu@ip-172-31-16-140.us-east-2.compute.internal>
2020-02-21 10:01:36 +0000

    Look again :/

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 // comments are in green
 // test of github ssh connection
+// try again :/
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
